5b66e3e8c4a86879ea724ed702d7288bd1aa236d
MatthewFrench <matthewf@ksu.edu>
2018-03-08 16:50:25 -0600

    Added final project

---
+++ Final Project/Common/Code/CSharp/Nodes/HexasphereNode.cs
@@ -0,0 +1,237 @@
+using Godot;
+using System;
+using FinalProject.Hexasphere;
+using System.Collections.Generic;
+
+public class HexasphereNode : Node
+{
+    List<Spatial> hexInstances = new List<Spatial>();
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        GD.Print("Hello from the Node CS Script Init");
+
+        //Define sphere properties
+        float radius = 95;
+        float scale = radius;
+        int subdivisionCount = 3;
+
+        //h is used for creating full size tiles
+        Hexasphere h = new Hexasphere((decimal)radius, subdivisionCount, 1);
+        //h2 is for creating the line mesh
+        Hexasphere h2 = new Hexasphere((decimal)radius, subdivisionCount, 1);
+
+        GD.Print("Number of Tiles: " + h.GetTiles().Count);
+        //Load the instances to put in the scene
+        var meshInstance = (PackedScene)ResourceLoader.Load("res://Common/Assets/MeshInstance.tscn");
+        var sphereMeshScene = (PackedScene)ResourceLoader.Load("res://Common/Assets/Blue Sphere.tscn");
+        var greenSphereMeshScene = (PackedScene)ResourceLoader.Load("res://Common/Assets/Green Sphere.tscn");
+        var redSphereMeshScene = (PackedScene)ResourceLoader.Load("res://Common/Assets/Red Sphere.tscn");
+        var hexagonTestScene = (PackedScene)ResourceLoader.Load("res://Hexagon Levels/Generic Square Maze/Generic Maze.tscn"); //Hexagon
+        var pentagonTestScene = (PackedScene)ResourceLoader.Load("res://Common/Assets/Pentagon.tscn"); //Pentagon, Main
+        var numberOfTiles = h.GetTiles().Count;
+        //Create all the tiles
+        foreach (var tile in h.GetTiles()) {
+            CreateInstance(tile, hexagonTestScene, pentagonTestScene, redSphereMeshScene, radius / numberOfTiles);
+        }
+        //Create the line mesh
+        foreach (var tile in h2.GetTiles())
+        {
+            CreateMesh(tile, sphereMeshScene, greenSphereMeshScene, h2.GetTiles().Count, radius*0.99f);
+        }
+    }
+
+    // Creates the hexagon or pentagon instance for the tile.
+    public void CreateInstance(Tile tile, PackedScene hexagonTestScene, PackedScene pentagonTestScene, PackedScene redSphereMeshScene, float sphereScale) {
+        decimal tileCenterX = 0;
+        decimal tileCenterY = 0;
+        decimal tileCenterZ = 0;
+        decimal polygonRadius = 0;
+
+        List<Point> points = tile.boundary;
+
+        //Calculate the average center point and polygon side length.
+        var lastPoint = points[points.Count - 1];
+        Vector3 lastPointVector = new Vector3((float)lastPoint.x, (float)lastPoint.y, (float)lastPoint.z);
+        decimal polygonSideLength = 0;
+        foreach (Point point in points)
+        {
+            tileCenterX += point.x / points.Count;
+            tileCenterY += point.y / points.Count;
+            tileCenterZ += point.z / points.Count;
+            Vector3 currentVector = new Vector3((float)point.x, (float)point.y, (float)point.z);
+            polygonSideLength += (decimal)currentVector.DistanceTo(lastPointVector);
+            lastPointVector = currentVector;
+        }
+        polygonSideLength = polygonSideLength / points.Count;
+
+        //Create the center point
+        var tileCenterPoint = new Vector3((float)tileCenterX, (float)tileCenterY, (float)tileCenterZ);
+        var firstPoint = new Vector3((float)points[0].x, (float)points[0].y, (float)points[0].z);
+
+        //Get the average polygon radius from center to each point.
+        foreach (Point point in points)
+        {
+            var vector = new Vector3((float)point.x, (float)point.y, (float)point.z);
+            polygonRadius += (decimal)vector.DistanceTo(tileCenterPoint);
+        }
+        polygonRadius = polygonRadius / points.Count;
+
+        var sphereCenterPoint = new Vector3(0f, 0f, 0f);
+
+        //Create the tile instance
+        Spatial groundTest = null;
+        if (points.Count == 5)
+        {
+            GD.Print("Pentagon Radius: " + polygonRadius + ", side length: " + polygonSideLength);
+
+            //Create instance
+            groundTest = (Spatial)pentagonTestScene.Instance();
+        }
+        else if (points.Count == 6)
+        {
+            GD.Print("Hexagon Radius: " + polygonRadius + ", side length: " + polygonSideLength);
+
+            //Create instance
+            groundTest = (Spatial)hexagonTestScene.Instance();
+        }
+        else
+        {
+            GD.Print("Unknown Radius: " + polygonRadius + ", Count: " + points.Count + ", Side Length: " + polygonSideLength);
+        }
+        if (groundTest != null)
+        {
+            //Put the instance on the tile and make it face the center of the sphere
+            //This should be all that is needed but the sections are rotated perpendicular
+            groundTest.LookAtFromPosition(tileCenterPoint, sphereCenterPoint, new Vector3(0f, 1f, 0f));
+            //Get the local axis of the instance
+            var axis = groundTest.GetTransform().basis.Xform(new Vector3(-1, 0, 0)).Normalized();
+            //Rotate it to face the center (I have no idea why this is necessary)
+            groundTest.Rotate(axis, (float)(Math.PI / 2.0));
+            //Get the local axis of the instance
+            var axis2 = groundTest.GetTransform().basis.Xform(new Vector3(0, 1, 0)).Normalized();
+            //Add ground to world
+            this.AddChild(groundTest);
+            this.hexInstances.Add(groundTest);
+
+            //Create and convert local point to world point
+            var localOriginalFirstPoint = new Vector3(0, 0, (float)polygonRadius);
+            var worldOriginalFirstPoint = groundTest.ToGlobal(localOriginalFirstPoint);
+
+            var worldGeneratedFirstPoint = firstPoint;
+            var localGeneratedFirstPoint = groundTest.ToLocal(worldGeneratedFirstPoint);
+
+            MeshInstance sphere = (MeshInstance)redSphereMeshScene.Instance();
+            sphere.SetTranslation(worldOriginalFirstPoint);
+            sphere.SetScale(new Vector3(sphereScale, sphereScale, sphereScale));
+            this.AddChild(sphere);
+            //World Original First Point works and is correct
+            //Now is there a way I can get the two points and match them up?
+            var angleCalcOriginalPoint = worldOriginalFirstPoint - tileCenterPoint;
+            var angleCalcFirstPoint = firstPoint - tileCenterPoint;
+
+            var angle = angleCalcFirstPoint.AngleTo(angleCalcOriginalPoint);
+            var angle2 = new Vector2(localGeneratedFirstPoint.x, localGeneratedFirstPoint.z).AngleTo(new Vector2(localOriginalFirstPoint.x, localOriginalFirstPoint.z));
+
+
+            var axis3 = groundTest.GetTransform().basis.Xform(new Vector3(0, 1, 0));
+            if (points.Count == 5) {
+                angle2 += (float)Math.PI / 4;
+            }
+            //angleCalcFirstPoint.
+            //Rotate it to match up with other parts of the sphere
+            groundTest.Rotate(axis3, angle2);
+
+
+
+            var newWorldPoint = groundTest.ToGlobal(localOriginalFirstPoint);
+            GD.Print("Difference: " + (worldGeneratedFirstPoint - newWorldPoint));
+        }
+    }
+
+    // Creates a wire mesh and places the spheres to make sure the tile is aligned.
+    public void CreateMesh(Tile tile, PackedScene sphereMeshScene, PackedScene greenSphereMeshScene, int numberOfTiles, float radius) {
+        var surfTool = new SurfaceTool();
+        var mesh = new ArrayMesh();
+        var material = new SpatialMaterial();
+        material.SetEmission(new Color(1.0f, 0.0f, 0.0f));
+        material.SetAlbedo(new Color(1.0f, 0.0f, 0.0f));
+        surfTool.SetMaterial(material);
+        surfTool.Begin(Mesh.PrimitiveType.LineLoop);
+        decimal tileCenterX = 0;
+        decimal tileCenterY = 0;
+        decimal tileCenterZ = 0;
+        decimal polygonRadius = 0;
+
+        List<Point> points = tile.boundary;
+        var lastPoint = points[points.Count - 1];
+        Vector3 lastPointVector = new Vector3((float)lastPoint.x, (float)lastPoint.y, (float)lastPoint.z);
+        decimal polygonSideLength = 0;
+        foreach (Point point in points) {
+            surfTool.AddUv(new Vector2(0, 0));
+            surfTool.AddVertex(new Vector3((float)point.x, (float)point.y, (float)point.z));
+
+            tileCenterX += point.x / points.Count;
+            tileCenterY += point.y / points.Count;
+            tileCenterZ += point.z / points.Count;
+            Vector3 currentVector = new Vector3((float)point.x, (float)point.y, (float)point.z); 
+            polygonSideLength += (decimal)currentVector.DistanceTo(lastPointVector);
+            lastPointVector = currentVector;
+        }
+        polygonSideLength = polygonSideLength / points.Count;
+
+        var tileCenterPoint = new Vector3((float)tileCenterX, (float)tileCenterY, (float)tileCenterZ);
+        var firstPoint = new Vector3((float)points[0].x, (float)points[0].y, (float)points[0].z);
+
+        foreach (Point point in points)
+        {
+            var vector = new Vector3((float) point.x, (float)point.y, (float)point.z);
+            polygonRadius += (decimal)vector.DistanceTo(tileCenterPoint);
+        }
+        polygonRadius = polygonRadius / points.Count;
+
+        var polygonRotation = firstPoint.AngleTo(tileCenterPoint);
+
+
+        var sphereCenterPoint = new Vector3(0f, 0f, 0f);
+
+        var sphereScale = radius / numberOfTiles;
+        MeshInstance sphere = (MeshInstance)sphereMeshScene.Instance();
+        sphere.SetTranslation(tileCenterPoint);
+        sphere.SetScale(new Vector3(sphereScale, sphereScale, sphereScale));
+        this.AddChild(sphere);
+
+        MeshInstance sphere2 = (MeshInstance)greenSphereMeshScene.Instance();
+        sphere2.SetTranslation(firstPoint);
+        sphere2.SetScale(new Vector3(sphereScale, sphereScale, sphereScale));
+        this.AddChild(sphere2);
+
+        MeshInstance sphere3 = (MeshInstance)greenSphereMeshScene.Instance();
+        sphere3.SetTranslation((firstPoint - tileCenterPoint) / 2 + tileCenterPoint);
+        sphere3.SetScale(new Vector3(sphereScale, sphereScale, sphereScale));
+        this.AddChild(sphere3);
+
+        surfTool.GenerateNormals();
+        surfTool.Index();
+        surfTool.Commit(mesh);
+        var meshInstance = new MeshInstance();
+        meshInstance.SetMesh(mesh);
+        this.AddChild(meshInstance);
+    }
+
+    //Not used currently
+    public override void _Process(float delta)
+    {
+        // Called every frame. Delta is time since last frame.
+        // Update game logic here.
+        /*
+        foreach (Spatial node in hexInstances) {
+            //Get the local axis of the instance
+            var axis = node.GetTransform().basis.Xform(new Vector3(0, 1, 0)).Normalized();
+            //Rotate it to match up with other parts of the sphere
+            node.Rotate(axis, (float)(1.0 / 180 * Math.PI));
+        }
+        */
+    }
+}
---
+++ Final Project/Common/Code/CSharp/Utility/Intersection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Godot;
+using System.Collections.Generic;
+
+namespace FinalProject.Utility
+{
+    public static class Intersection
+    {
+        //Returns true if polygons intersect.
+        public static bool DoPolygonsIntersect(List<Vector2> polygon1, List<Vector2> polygon2) {
+            for (var polygon1Index = 0; polygon1Index < polygon1.Count; polygon1Index++) {
+                var polygon1Point1 = polygon1[polygon1Index];
+                Vector2 polygon1Point2;
+                if (polygon1Index > 0) {
+                    polygon1Point2 = polygon1[polygon1Index - 1];
+                } else {
+                    polygon1Point2 = polygon1[polygon1.Count - 1];
+                }
+
+                for (var polygon2Index = 0; polygon2Index < polygon2.Count; polygon2Index++)
+                {
+                    var polygon2Point1 = polygon2[polygon2Index];
+                    Vector2 polygon2Point2;
+                    if (polygon2Index > 0)
+                    {
+                        polygon2Point2 = polygon2[polygon2Index - 1];
+                    }
+                    else
+                    {
+                        polygon2Point2 = polygon2[polygon2.Count - 1];
+                    }
+                    if (DoLineSegmentsIntersection(polygon1Point1, polygon1Point2, polygon2Point1, polygon2Point2)) {
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+
+        //Returns true if the lines intersect.
+        public static bool DoLineSegmentsIntersection(Vector2 point1, Vector2 point2, Vector2 point3, Vector2 point4)
+        {
+            return DoLineSegmentsIntersection(point1.x, point1.y, point2.x, point2.y,
+                                              point3.x, point3.y, point4.x, point4.y);
+        }
+
+        //https://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+        //Returns true if the lines intersect.
+        public static bool DoLineSegmentsIntersection(float p0_x, float p0_y, float p1_x, float p1_y,
+            float p2_x, float p2_y, float p3_x, float p3_y)
+        {
+            //float i_x;
+            //float i_y;
+            float s1_x, s1_y, s2_x, s2_y;
+            s1_x = p1_x - p0_x; s1_y = p1_y - p0_y;
+            s2_x = p3_x - p2_x; s2_y = p3_y - p2_y;
+
+            float s, t;
+            s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
+            t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
+
+            if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
+            {
+                // Collision detected
+                //if (i_x != NULL)
+                //    i_x = p0_x + (t * s1_x);
+                //if (i_y != NULL)
+                //    i_y = p0_y + (t * s1_y);
+                return true;
+            }
+
+            return false; // No collision
+        }
+    }
+}
