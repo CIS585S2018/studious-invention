38f8a408e98bb629a5b7fc8321dd85fad5ebd856
Filip Volf <volf.filip@gmail.com>
2018-04-03 15:27:32 -0500

    Changes in player - added hands

--- Final Project/Common/Code/GDScript/player.gd
+++ Final Project/Common/Code/GDScript/player.gd
@@ -8,9 +8,9 @@ var velocity = Vector3()
 #camera control
 var camera
 var third_person = false
-var view_sensitivity = 0.15
-var yaw = 0
-var pitch = 0
+#var view_sensitivity = 0.15
+#var yaw = 0
+#var pitch = 0
 var flashlight
 
 var pos_label
@@ -20,9 +20,9 @@ var pos_label
 func _ready():
 	set_process_input(true)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	camera = get_node("yaw/Camera")
+	camera = get_node("yaw/CameraBase/Camera")
 	self.set_meta("name","player")
-	flashlight = get_node("yaw/Camera/SpotLight")
+	flashlight = get_node("yaw/CameraBase/Camera/SpotLight")
 	flashlight.visible = false
 
 func _enter_scene():
@@ -36,15 +36,16 @@ func _physics_process(delta):
 	
 	direction = Vector3()
 	if Input.is_action_pressed("ui_left") or Input.is_key_pressed(KEY_A):
-		direction -= aim[0]
+		direction -= Vector3(1,0,0);
 	if Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D):
-		direction += aim[0]
+		direction += Vector3(1,0,0);
 	if Input.is_action_pressed("ui_up") or Input.is_key_pressed(KEY_W):
-		direction -= aim[2]
+		direction -= Vector3(0,0,1);
 	if Input.is_action_pressed("ui_down") or Input.is_key_pressed(KEY_S):
-		direction += aim[2]
+		direction += Vector3(0,0,1);
 	
-	direction = direction.normalized()
+	direction = camera.global_transform.basis.xform(direction);
+	direction = direction.normalized();
 	direction = direction * speed * delta
 	
 	if velocity.y > 0:
@@ -65,7 +66,7 @@ func _physics_process(delta):
 		
 # cast a short ray and call the use() method if the object we hit is usable
 func use_thing():
-	var ray = self.get_node("yaw/Camera/ray")
+	var ray = self.get_node("yaw/CameraBase/Camera/ray")
 	if ray.is_colliding():
 		var object = ray.get_collider()
 		var type = object.get_meta("type")
@@ -76,7 +77,7 @@ func use_thing():
 	
 # cast a ray (a much longer one than the "use item" ray), call onDeath() if the object being hit is killable
 func shoot():
-	var gun = self.get_node("yaw/Camera/gun")
+	var gun = self.get_node("yaw/CameraBase/Camera/gun")
 	if gun.is_colliding():
 		var object = gun.get_collider()
 		var type = object.get_meta("type") # spams errors if the object doesn't have a type set, doesn't crash tho
@@ -121,8 +122,8 @@ func _input(ie):
 		flashlight.visible = not flashlight.visible
 		pass
 	#mouse movement
-	if ie is InputEventMouseMotion:
-		yaw = fmod(yaw - ie.relative.x * view_sensitivity, 360)
-		pitch = max(min(pitch - ie.relative.y * view_sensitivity, 90), -90)
-		set_rotation(Vector3(0, deg2rad(yaw), 0))
-		camera.set_rotation(Vector3(deg2rad(pitch), 0, 0))
+	#if ie is InputEventMouseMotion:
+	#	yaw = fmod(yaw - ie.relative.x * view_sensitivity, 360)
+	#	pitch = max(min(pitch - ie.relative.y * view_sensitivity, 90), -90)
+	#	set_rotation(Vector3(0, deg2rad(yaw), 0))
+	#	camera.set_rotation(Vector3(deg2rad(pitch), 0, 0))
