3d7b9b76c492fed77a247c5f5f108e44067cf77c
Filip Volf <volf.filip@gmail.com>
2018-04-21 15:32:38 -0500

    Changed the character from rigid body to kinematic body again, even though rigid body is better solution, the collision detections are too demanding on processing power. The movemenet/jump/gravity speed of the player can be easily changed using the variables at the top in the script player.gd

--- Final Project/Common/Code/GDScript/player.gd
+++ Final Project/Common/Code/GDScript/player.gd
@@ -1,11 +1,12 @@
-extends RigidBody
+extends KinematicBody
 
-var moveSpeed = 1000;
-var jumpForce = 10;
-var gravity = 10;
+var mult = 0.075;
+var moveSpeed = 100 * mult;
+var jumpForce = 12.5 * mult;
+var gravity = 200 * mult;
 var m_justJumping = false;
 var gravityCompounded = Vector3();
-var max_speed = 5;
+var max_speed = 999999 * mult;
 #camera control
 var camera
 var third_person = false
@@ -15,6 +16,7 @@ var song=0
 var can_change_fullscreen = true;
 var fullscreen = false;
 var gravity_vec
+var finalMoveVector = Vector3();
 
 onready var yaw = get_node("../yaw")
 
@@ -34,14 +36,43 @@ func _enter_scene():
 func _exit_scene():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func _integrate_forces(state):
+func _physics_process(delta):
+	var g_norm = get_global_transform().origin.normalized();
+	gravity_vec = g_norm  * gravity;
+	var mMoveDir = calculate_move_dir(); 
+	finalMoveVector += gravity_vec*delta;
+	
+	move_and_slide(finalMoveVector,-g_norm);
+	
+	#if(is_on_floor()):
+	move_and_slide(mMoveDir,-g_norm);
+	
+	if(is_on_floor()):
+		finalMoveVector = Vector3();
+	
+	if(Input.is_key_pressed(KEY_SPACE)):
+		if(!m_justJumping):
+			finalMoveVector -= gravity_vec*jumpForce; 
+			m_justJumping = true;
+	else :
+		if(is_on_floor()):
+			if(m_justJumping):
+				m_justJumping = false;
+			
+	#if(finalMoveVector.length() > max_speed):
+	#	finalMoveVector = finalMoveVector.normalized()*max_speed;
+	
+
+		
+	
+"""func _integrate_forces(state):
 	
 	gravity_vec =  get_global_transform().origin.normalized() * gravity;
 	PhysicsServer.area_set_param(get_world().get_space(), PhysicsServer.AREA_PARAM_GRAVITY_VECTOR , gravity_vec);
 	
 	#finalMoveVector is the vector that combines all forces to one
 	var mMoveDir = calculate_move_dir(); 
-	var finalMoveVector = state.get_linear_velocity()*0.96 + (mMoveDir + gravity_vec)*state.get_step();
+	var finalMoveVector = state.get_linear_velocity()*0.99 + gravity_vec*state.get_step();
 	
 	#jumping
 	if(Input.is_key_pressed(KEY_SPACE)):
@@ -57,12 +88,30 @@ func _integrate_forces(state):
 		finalMoveVector = finalMoveVector.normalized()*max_speed;
 	
 	#apply force
-	state.set_linear_velocity(finalMoveVector);
+	var tmp_vec3 = Vector3();
+	
+	if(contact_monitor):
+		tmp_vec3 =  mMoveDir*state.get_step();
+		
+	state.set_linear_velocity(finalMoveVector + tmp_vec3 );"""
 
 func calculate_move_dir():
+	
+	var g_norm = get_global_transform().origin.normalized();
+	
 	var steer = float(Input.is_key_pressed(KEY_D))-float(Input.is_key_pressed(KEY_A));
 	var straight = float(Input.is_key_pressed(KEY_S))-float(Input.is_key_pressed(KEY_W));
-	return camera.global_transform.basis.xform(Vector3(steer,0,straight).normalized())*moveSpeed;
+	
+	var x_axis = camera.global_transform.basis.x;
+	var y_axis = camera.global_transform.basis.y;
+	var z_axis = camera.global_transform.basis.z;
+	
+	var straightVec3 = g_norm.rotated(x_axis,-PI/2*straight);
+	var steerVec3 = g_norm.rotated(z_axis,PI/2*steer);
+	
+	var fin = (straightVec3 + steerVec3).normalized();
+	
+	return fin*moveSpeed;
 	
 # cast a short ray and call the use() method if the object we hit is usable
 func use_thing():
